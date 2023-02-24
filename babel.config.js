1d551ada54ecd5c3cbc48e71adf5221a774553b2
Bogdan Starchuk <bstarchuk95@gmail.com>
2023-02-24 13:42:29 +0200

    Change npm to yarn

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    plugins: ['effector/babel-plugin'],
-    env: {
-        test: {
-            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-            plugins: ['@babel/transform-runtime', 'effector/babel-plugin'],
-        },
+  plugins: ['effector/babel-plugin'],
+  env: {
+    test: {
+      presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+      plugins: ['@babel/transform-runtime', 'effector/babel-plugin'],
     },
+  },
 };
